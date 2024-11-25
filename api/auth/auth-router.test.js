9250acdc71993dfbe53658991363e68b9e0b61cd
Robert Jank <robert.jank@yahoo.com>
2024-11-25 14:08:37 -0600

    wip

--- api/auth/auth-router.test.js
+++ api/auth/auth-router.test.js
@@ -1,6 +1,6 @@
 const db = require("../../data/dbConfig");
 const request = require("supertest");
-const server = require("./auth-router");
+const server = require("../server");
 
 beforeAll(async () => {
   await db.migrate.rollback();
@@ -12,8 +12,24 @@ describe("[POST] /register", () => {
     username: "Captain Marvel",
     password: "foobar",
   };
+  const newUser2 = {
+    username: "Bat Man",
+    password: "foobar",
+  };
+  const newUser3 = {
+    username: "Taco Man",
+    password: "foobar",
+  };
   test("adds a new user to the database", async () => {
-    await request(server).post("/register").send(newUser);
+    await request(server).post("/api/auth/register").send(newUser);
     expect(await db("users")).toHaveLength(1);
+    await request(server).post("/api/auth/register").send(newUser2);
+    expect(await db("users")).toHaveLength(2);
+  });
+  test("responds with the new user", async () => {
+    const res = await request(server).post("/api/auth/register").send(newUser3);
+    expect(res.body).toHaveProperty("username");
+    expect(res.body).toHaveProperty("password");
+    expect(res.body.username).toEqual(newUser3.username);
   });
 });
